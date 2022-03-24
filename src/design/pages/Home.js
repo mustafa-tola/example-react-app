540b616418237b2ef16359198b9efa0693ac9b56
syedabdullah12 <76275230+syedabdullah12@users.noreply.github.com>
2022-03-24 19:54:37 +0500

    MADE LOGIN/SIGNUP PAGE
    MADE LOGIN/SIGNUP PAGE

--- src/design/pages/Home.js
+++ src/design/pages/Home.js
@@ -1,49 +1,173 @@
 import React from 'react'
-import {Button,Paper} from "@material-ui/core"
+import {Button,Paper,ButtonGroup} from "@material-ui/core"
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import WidgetsRoundedIcon from '@material-ui/icons/WidgetsRounded';
+import Avatar from '@material-ui/core/Avatar';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+
+import Container from '@material-ui/core/Container';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+
+const useStyles = makeStyles((theme) => ({
+  
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 3),
+  },
+}));
 
 export const Home = () => {
+  const classes = useStyles();
   return (
       <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" style={{"color":"white"}} href="#"><WidgetsRoundedIcon  /></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="#" style={{"color":"white","fontSize":"30px"}}>Trello</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"/></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
+          
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
+        <form class="d-flex" >
+          
+        <TextField id="standard-basic"  placeholder="Search" style={{"color":"white","padding":"0"}}/>
+        <Button variant="contained" color="primary" size="small" className={classes.margin}>Search</Button>
+          
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" size="small" className={classes.margin}>
+            <Button >LOGIN</Button>
+            <Button>SIGN UP</Button>
+            
+          </ButtonGroup>
+          
         </form>
       </div>
     </div>
   </nav>
-  
-    <Paper elevation={3}>
-        <h1>My name is mustafa</h1>
-    </Paper>
+  <Container component="main" maxWidth="xs" style={{"backgroundColor":"#DAFFFA"}}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <VpnKeyIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} style={{}}>
+              <TextField autoComplete="fname" name="firstName" required fullWidth id="firstName" label="First Name"
+                autoFocus color="warning" varient="standard"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                color="warning" varient="standard"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+               
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            
+          </Grid>
+          <Button
+            type="submit"
+            style={{"float":"centre"}}
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      {/* <Box mt={5}>
+        <Copyright />
+      </Box> */}
+    </Container>
+  );
+
+   
     </div>
   )
 }
